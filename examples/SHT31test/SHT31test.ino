b5930b7434029e4e50efa4e9de716fee4dbb7fab
ladyada <limor@ladyada.net>
2015-12-08 17:01:06 -0500

    demo code

---
+++ examples/SHT31test/SHT31test.ino
@@ -0,0 +1,33 @@
+/*************************************************** 
+  This is an example for the SHT31-D Humidity & Temp Sensor
+
+  Designed specifically to work with the SHT31-D sensor from Adafruit
+  ----> https://www.adafruit.com/products/2857
+
+  These sensors use I2C to communicate, 2 pins are required to  
+  interface
+ ****************************************************/
+ 
+#include <Arduino.h>
+#include <Wire.h>
+#include "Adafruit_SHT31.h"
+
+Adafruit_SHT31 sht31 = Adafruit_SHT31();
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("SHT31 test");
+  if (! sht31.begin(0x44)) {   // Set to 0x45 for alternate i2c addr
+    Serial.println("Couldn't find SHT31");
+    while (1) delay(1);
+  }
+}
+
+
+void loop() {
+  Serial.print("Temp *C = "); Serial.println(sht31.readTemperature());
+  Serial.print("Hum. % = "); Serial.println(sht31.readHumidity());
+
+  Serial.println();
+  delay(1000);
+}
