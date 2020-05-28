e6cd95c19ad9b0d676e4f881332660c39a9c18dc
Doxygen CI <travis@travis-ci.org>
2020-05-28 02:55:42 +0000

    Deploy code docs to GitHub Pages Travis build:
    Commit:

--- html/search/all_2.js
+++ html/search/all_2.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['begin',['begin',['../class_adafruit___s_h_t31.html#a0bb88d16363728dc468beef68b0801d4',1,'Adafruit_SHT31']]]
+  ['heater',['heater',['../class_adafruit___s_h_t31.html#a703abea4a6634ed56e13bab2dc5730fb',1,'Adafruit_SHT31']]]
 ];
