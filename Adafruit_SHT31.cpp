c60e6555b9e79cfe4b8f9c1c6f8b3417325d483d
M@x <Max@max.com>
2020-09-27 17:15:36 +0200

    Use of SHT31-D datasheet temperature conversion formula

--- Adafruit_SHT31.cpp
+++ Adafruit_SHT31.cpp
@@ -189,6 +189,8 @@ bool Adafruit_SHT31::readTempHum(void) {
   int32_t stemp = (int32_t)(((uint32_t)readbuffer[0] << 8) | readbuffer[1]);
   // simplified (65536 instead of 65535) integer version of:
   // temp = (stemp * 175.0f) / 65535.0f - 45.0f;
+  stemp = ((175.0f * (float)stemp) / 65535.0f) - 45.0f;
+  temp = (float)stemp;
   stemp = ((4375 * stemp) >> 14) - 4500;
   temp = (float)stemp / 100.0f;
 
